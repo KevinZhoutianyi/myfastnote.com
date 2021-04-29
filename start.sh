36985aa36358636cb768c5ad4d92d700406595a2
KevinZhoutianyi <kevinzhou2@live.cn>
2021-04-29 16:11:08 +0800

    nothing

--- start.sh
+++
@@ -1,2 +0,0 @@
-forever start -a -l /home/git/myfastnote.com/tmp/forever.log -o /home/git/myfastnote.com/tmp/out.log -e /home/git/myfastnote.com/tmp/err.log index.js  
-echo "Server start"
--- stop.sh
+++
@@ -1,4 +0,0 @@
-forever stopall
-echo "" > ./tmp/err.log
-echo "" > ./tmp/forever.log
-echo "" > ./tmp/out.log
